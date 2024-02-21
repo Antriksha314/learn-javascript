686f13fe28bbecd2456cb74e995ac208f7fb91ed
Antriksha Rattan <antriksharattan314@gmail.com>
2024-02-21 19:11:14 +0530

    Applied new method for concatination string

--- experiments/js/index.js
+++ experiments/js/index.js
@@ -7,6 +7,10 @@ console.log(yearlyRent);
 const firstName = "Brian";
 const lastName = "Holt";
 
-const sentence = "Hello " + firstName + " " + lastName + "! How are you?!";
+const concatinateString = "Hello " + firstName + " " + lastName + "! How are you?!";
+
+console.log(concatinateString);
+
+const sentence = `Hello ${firstName} ${lastName}! How are you?!`;
 
 console.log(sentence);
