d2e3766db88ae8124786873a511866850decc215
Antriksha Rattan <antriksharattan314@gmail.com>
2024-02-21 19:03:43 +0530

    Day 1 javascript

---
+++ experiments/js/index.js
@@ -0,0 +1,12 @@
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent * 12;
+
+console.log(yearlyRent);
+
+const firstName = "Brian";
+const lastName = "Holt";
+
+const sentence = "Hello " + firstName + " " + lastName + "! How are you?!";
+
+console.log(sentence);
